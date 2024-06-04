0b692ed41ae7e8951eabb31edf39558755565049
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-06-04 21:26:08 +0530

    adding daisyui

---
+++ login-with-mongo/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <button class="btn">Button</button>
+            <button class="btn btn-primary">Button</button>
+
+        </div>
+    )
+}
+
+export default Navbar
